dc98fc67dc8bc8b6e5a9c3967aeb1e77ccc372d5
ali <loading.03@mail.ru>
2023-07-11 13:51:54 +0300

    Начал вёрстку

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import styles from './index.module.css'
+import Header from "./components/Header"
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div className="wrapper">
+      <Header />
+    </div>
   )
 }
 
