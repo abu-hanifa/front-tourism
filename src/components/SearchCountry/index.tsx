323cdf0722d4224fd3a1b701bf713eb2166ef024
Djamalayl <djamalayl32@mail.ru>
2023-07-12 11:22:22 +0300

    className

--- src/components/SearchCountry/index.tsx
+++ src/components/SearchCountry/index.tsx
@@ -10,10 +10,10 @@ function SearchCountry() {
       {/* <div className={style.search_country}> */}
 
       <div className={style.input_flex}>
-        <div class='input-group mb-3' >
+        <div className='input-group mb-3' >
           <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
           <div className="input-group-append">
-            <button class="btn btn-outline-secondary" type="button">Button</button>
+            <button className="btn btn-outline-secondary" type="button">Button</button>
           </div>
         </div>
       </div>
