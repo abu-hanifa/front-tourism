a90b745dff8dfeab6585e52b3ece86783a54e676
Djamalayl <djamalayl32@mail.ru>
2023-07-12 10:24:32 +0300

    add index

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,27 +8,24 @@ function Header() {
       <div className={style.nav_bar}>
         <ul>
           <li>
-            <a href="/">Текст 1</a>
+            <a href="/">Publics</a>
           </li>
           <li>
             <a href="/">Текст 2</a>
           </li>
-        </ul>
-        <div>
-          <img src={logo} alt="логотип" />
-        </div>
-        <ul>
+
+          <li>
+            <img src={logo} alt="логотип" />
+          </li>
+
           <li>
             <a href="/">Текст 3</a>
-          </li>
-          <li>
-            <a href="/">Текст 4</a>
           </li>
         </ul>
       </div>
       <div className={style.profile}>
         <img src={user} alt="профиль" />
-        <span>User_name</span>
+        <span>Log in</span>
       </div>
     </header>
   );
