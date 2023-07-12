8d3143e1f5042e5c06d1e4480edea0861019ca4a
Djamalayl <djamalayl32@mail.ru>
2023-07-12 11:42:06 +0300

    new main

--- src/components/SearchCountry/index.tsx
+++ src/components/SearchCountry/index.tsx
@@ -7,9 +7,10 @@ function SearchCountry() {
   return (
     <div className={style.search}>
       <span>Где бы вы хотели отдохнуть?</span>
-      {/* <div className={style.search_country}> */}
+    
 
-      <div className={style.input_flex}>
+  <div className={style.input_center}>
+  <div className={style.input_flex}>
         <div className='input-group mb-3' >
           <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
           <div className="input-group-append">
@@ -17,15 +18,10 @@ function SearchCountry() {
           </div>
         </div>
       </div>
+  </div>
 
 
-      {/*      
-        <div className={style.buttogin}>
-          <input type="text" />
-          <img width={35} src={button} alt="поиск" />
-          <img className={style.button_icon} src={lupa} alt="лупа" />
-        </div> */}
-      {/* </div> */}
+    
     </div>
   );
 }
